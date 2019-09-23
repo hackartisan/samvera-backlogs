2ef320649f6f4be9155c7a6dda6877064f306c8e
Anna Headley <anna3LC@gmail.com>
2019-09-23 16:00:49 -0400

    Created a main method
    expanded graphql query
    added pullRequests query

--- github_graphql_client.rb
+++ github_graphql_client.rb
@@ -3,9 +3,8 @@ require "json"
 
 DATA_DIR = "data"
 API_URL = "https://api.github.com/graphql"
-
-token = ENV["GITHUB_TOKEN"] || "you_need_a_token"
-puts token
+TOKEN = ENV["GITHUB_TOKEN"] || "you_need_a_token"
+puts TOKEN
 
 class GithubGraph
   def initialize(token:)
@@ -19,20 +18,51 @@ class GithubGraph
     query {
       organization(login: "pulibrary") {
         repository(name: "figgy") {
-          issues(first:1) {
+          issues(first:2) {
             nodes {
-              createdAt
-              bodyText
+              comments
+              labels
+              number
               title
+              bodyText
               closed
+              milestone
+              createdAt
             }
           }
         }
       }
     }
     GRAPHQL
-    puts response.data.issues
+    response
   end
+
+  # parse out org and repo name
+  # return an array of json text objects?
+  def download_prs(repository_url:, dir:)
+    response = client.query <<~GRAPHQL
+    query {
+      organization(login: "pulibrary") {
+        repository(name: "figgy") {
+          pullRequests(first:2) {
+            nodes {
+              comments
+              labels
+              number
+              title
+              bodyText
+              merged
+              milestone
+              createdAt
+            }
+          }
+        }
+      }
+    }
+    GRAPHQL
+    response
+  end
+
 
   private
 
@@ -63,7 +93,9 @@ class RepositoryList
   end
 end
 
-repos = RepositoryList.new(path: "repositories.json").parse
-puts repos.first
-github = GithubGraph.new(token: token)
-github.download_issues(repository_url: repos.first[:url], dir: DATA_DIR)
+def run
+  repos = RepositoryList.new(path: "repositories.json").parse
+  puts repos.first
+  github = GithubGraph.new(token: TOKEN)
+  github.download_issues(repository_url: repos.first[:url], dir: DATA_DIR)
+end
