1b25734a701c0d8cd45238255dd722551bb0b930
Anna Headley <anna3LC@gmail.com>
2019-09-24 15:38:33 -0400

    create downloader class

--- github_graphql_client.rb
+++ github_graphql_client.rb
@@ -6,15 +6,32 @@ API_URL = "https://api.github.com/graphql"
 TOKEN = ENV["GITHUB_TOKEN"] || "you_need_a_token"
 puts TOKEN
 
-class GithubGraph
-  def initialize(token:)
-    @token = token
+class Downloader
+  def initialize(client:, repository_url:, dir:)
+    @client = client
+    @repository_url = repository_url
+    @dir = dir
   end
 
   # parse out org and repo name
   # return an array of json text objects?
-  def download_issues(repository_url:, dir:)
-    response = client.query <<~GRAPHQL
+  def download_issues
+    # the first time we won't have a cursor
+    #cursor = nil
+    #while cursor
+    #  then run the thing
+    #  cursor = response_is_empty? ? nil : last_items_cursor
+    #end
+    download_issue_batch
+  end
+
+    # from then on we will have a cursor, which we need to pull out of the
+    #   response
+    # at some point the cursor will be some terminal value? so test for that?
+    # how do you know you're at the end?
+
+  def download_issue_batch
+    response = @client.query <<~GRAPHQL
     query {
       organization(login: "pulibrary") {
         repository(name: "figgy") {
@@ -43,8 +60,8 @@ class GithubGraph
 
   # parse out org and repo name
   # return an array of json text objects?
-  def download_prs(repository_url:, dir:)
-    response = client.query <<~GRAPHQL
+  def download_prs
+    response = @client.query <<~GRAPHQL
     query {
       organization(login: "pulibrary") {
         repository(name: "figgy") {
@@ -67,18 +84,22 @@ class GithubGraph
     response
   end
 
+end
 
-  private
+class GithubClient
+  def initialize(token:)
+    @token = token
+  end
 
-    def client
-      @client ||= Graphlient::Client.new(
-        API_URL,
-        headers: {
-          "Authorization" => "bearer #{@token}",
-          "Accept" => "application/vnd.github.hawkgirl-preview+json"
-        }
-      )
-    end
+  def client
+    @client ||= Graphlient::Client.new(
+      API_URL,
+      headers: {
+        "Authorization" => "bearer #{@token}",
+        "Accept" => "application/vnd.github.hawkgirl-preview+json"
+      }
+    )
+  end
 end
 
 class RepositoryList
@@ -101,9 +122,10 @@ def repos
   RepositoryList.new(path: "repositories.json").parse
 end
 
-def run
-  puts repos.first
-  github = GithubGraph.new(token: TOKEN)
-  # github.download_issues(repository_url: repos.first[:url], dir: DATA_DIR)
-  github
+def downloader
+  github = GithubClient.new(token: TOKEN).client
+  Downloader.new(client: github, repository_url: repos.first[:url], dir: DATA_DIR)
 end
+
+# do something like this with it:
+#downloader.download_issues
