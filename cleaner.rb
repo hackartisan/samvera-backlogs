06facfc4d95173bf6872b4efc7a0aef972bc93cc
Eliot Jordan <eliotj@princeton.edu>
2019-10-08 15:51:40 -0500

    Add document cleaner

--- cleaner.rb
+++ cleaner.rb
@@ -1,17 +1,14 @@
 require 'pry'
 
 path = File.join('data')
-
-# find all files
-# match /
-mention_regex = /(\B@[a-zA-Z]*)/g
-# add them to an array and dedup
-# print them
-
+mention_regex = /(\B@[a-zA-Z0-9]*)/
 all_matches = []
+
 Dir.glob("data/**/*.txt") do |fn|
   text = File.read(fn)
-  all_matches + text.scan(mention_regex)
+  all_matches << text.scan(mention_regex)
 end
 
-binding.pry
+all_matches.flatten.group_by { |i| i }.map { |k,v| [k, v.count] }.to_h.sort_by { |k,v| v }.each do |mentions|
+  puts(" #{mentions[1]}: #{mentions[0]}")
+end
