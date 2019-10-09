9e20f32230c14127ec0dd3a8e38e2de4655d22aa
Eliot Jordan <eliotj@princeton.edu>
2019-10-09 15:45:36 -0500

    Remove urls and email addresses

--- cleaner.rb
+++ cleaner.rb
@@ -15,7 +15,13 @@ def print_snail_terms
   end
 end
 
-# TODO: clean email addresses, urls?
+def email_regex
+  %r{(?:[a-z0-9!#$%&'*+\/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+\/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])}
+end
+
+def url_regex
+  URI::regexp(%w(http https))
+end
 
 def write_clean_documents
   ["open_issues", "recent_issues", "merged_prs"].each do |path|
@@ -28,7 +34,9 @@ def write_clean_documents
     text = File.read(fn)
 
     blacklist.each do |term|
-      text = text.gsub(term[0], "")
+      text.gsub!(term[0], "")
+      text.gsub!(email_regex, "")
+      text.gsub!(url_regex, "")
     end
 
     new_fn = fn.gsub('raw', 'clean')
