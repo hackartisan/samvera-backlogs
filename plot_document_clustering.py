c39f49b9535ccf7c5453f9c887f97ed795cbea9b
Anna Headley <anna3LC@gmail.com>
2019-10-10 16:13:41 -0400

    Add stopwords

--- plot_document_clustering.py
+++ plot_document_clustering.py
@@ -45,9 +45,39 @@ op.add_option("--verbose",
               action="store_true", dest="verbose", default=False,
               help="Print progress reports inside k-means algorithm.")
 
-print(__doc__)
-op.print_help()
+#print(__doc__)
+#op.print_help()
 
+base_stopwords = ["a", "about", "above", "after", "again", "against", "ain", "all",
+        "am", "an", "and", "any", "are", "aren", "aren't", "as", "at", "be",
+        "because", "been", "before", "being", "below", "between", "both", "but",
+        "by", "can", "couldn", "couldn't", "d", "did", "didn", "didn't", "do",
+        "does", "doesn", "doesn't", "doing", "don", "don't", "down", "during",
+        "each", "few", "for", "from", "further", "had", "hadn", "hadn't", "has",
+        "hasn", "hasn't", "have", "haven", "haven't", "having", "he", "her",
+        "here", "hers", "herself", "him", "himself", "his", "how", "i", "if",
+        "in", "into", "is", "isn", "isn't", "it", "it's", "its", "itself",
+        "just", "ll", "let", "m", "ma", "me", "mightn", "mightn't", "more",
+        "most", "mustn", "mustn't", "my", "myself", "needn", "needn't", "no",
+        "nor", "not", "now", "o", "of", "off", "on", "once", "only", "or",
+        "other", "our", "ours", "ourselves", "out", "over", "own", "re", "s",
+        "same", "shan", "shan't", "she", "she's", "should", "should've",
+        "shouldn", "shouldn't", "so", "some", "such", "t", "than", "that",
+        "that'll", "the", "their", "theirs", "them", "themselves", "then",
+        "there", "these", "they", "this", "those", "through", "to", "too",
+        "under", "until", "up", "ve", "very", "was", "wasn", "wasn't", "we",
+        "were", "weren", "weren't", "what", "when", "where", "which", "while",
+        "who", "whom", "why", "will", "with", "won", "won't", "wouldn",
+        "wouldn't", "y", "you", "you'd", "you'll", "you're", "you've", "your",
+        "yours", "yourself", "yourselves", "could", "he'd", "he'll", "he's",
+        "here's", "how's", "i'd", "i'll", "i'm", "i've", "let's", "ought",
+        "she'd", "she'll", "that's", "there's", "they'd", "they'll", "they're",
+        "they've", "we'd", "we'll", "we're", "we've", "what's", "when's",
+        "where's", "who's", "why's", "would"]
+corpus_stopwords = ["princeton", "pudl", "scholar", "uc", "scholar_uc_legacy",
+        "uclibs", "figgy", "psu", "scholarsphere", "hydrus", "acceptance",
+        "criteria"]
+stopwords = base_stopwords + corpus_stopwords
 
 def is_interactive():
     return not hasattr(sys.modules['__main__'], '__file__')
@@ -64,7 +94,7 @@ if len(args) > 0:
 # #############################################################################
 print("Loading open issues:")
 
-dataset = load_files("./data/open_issues")
+dataset = load_files("./data/open_issues/clean")
 # TODO: Shuffle the data
 
 print("%d documents" % len(dataset.data))
@@ -91,7 +121,7 @@ if opts.use_hashing:
                                        binary=False)
 else:
     vectorizer = TfidfVectorizer(max_df=0.5, max_features=opts.n_features,
-                                 min_df=2, stop_words='english',
+                                 min_df=2, stop_words=stopwords,
                                  use_idf=opts.use_idf)
 X = vectorizer.fit_transform(dataset.data)
 
